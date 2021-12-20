5959b63408097a8f3055468ad271669ac5e11b23
zhonghua <secondsquare@qq.com>
2021-12-20 16:15:36 +0800

    read eval print loop

--- main.cpp
+++ main.cpp
@@ -1,7 +1,36 @@
 #include <cstdio>
+#include <string>
+#include <iostream>
+
+int read(std::string &input)
+{
+	if(getline(std::cin, input))
+		return input.size();
+	else
+		return 0;
+}
+
+std::string eval(std::string &input)
+{
+	return input;
+}
+
+void print(std::string &result)
+{
+	printf("%s\n", result.c_str());
+}
 
 int main()
 {
-	printf("hello world");
+	printf("dummyscheme\n");
+	while(true) {
+		printf("user>");
+		std::string input;	
+		if(!read(input))
+			break;
+		std::string result = eval(input);	
+		print(result);
+	}
+	
 	return 0;
 }
