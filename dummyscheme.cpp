b7ab4c33cfe8353bc46c9bf73271effa1c5634d9
zhonghua <secondsquare@qq.com>
2021-12-22 15:43:32 +0800

    read symbol and half readform

--- dummyscheme.cpp
+++ dummyscheme.cpp
@@ -1,5 +1,14 @@
 #include "dummyscheme.h"
 
+void unexpectedToken(std::string &input, int startIndex)
+{
+		std::stringstream ss;
+		ss << "unexpected token "
+			 << input[startIndex]
+			 << " at " << startIndex;
+		printf(ss.str());
+}
+
 int skipBlank(std::string &input, int startIndex)
 {
 	while(startIndex < input.size())
@@ -18,4 +27,39 @@ int skipBlank(std::string &input, int startIndex)
 	}
 
 	return -1;
+}
+
+int readSymbol(std::string &input, int startIndex)
+{
+	std::stringstream symbol;
+	if (!(input[startIndex] >= 'a' && input[startIndex] <= 'z') &&
+			!(input[startIndex] >= 'A' && input[startIndex] <= 'Z'))
+	{
+		unexpectedToken(input, startIndex);
+		return -1;
+	}
+
+	symbol << input[startIndex++];
+	while((input[startIndex] >= 'a' && input[startIndex] <= 'z') ||
+				(input[startIndex] >= 'A' && input[startIndex] <= 'Z') ||
+				(input[startIndex] >= '0' && input[startIndex] <= '9') ||
+				input[startIndex] == '-')
+	{
+		symbol << input[startIndex++];
+	}
+
+	return startIndex;
+}
+
+int readForm(std::string &input, int startIndex)
+{
+	if (input[startIndex] != TOKEN_LEFT_PAREN)
+	{
+		unexpectedToken(input, startIndex);
+		return -1;
+	}
+
+	startIndex++;
+	startIndex = skipBlank(input, startIndex);
+	startIndex = readSymbol(input, startIndex);
 }
--- dummyscheme.h
+++ dummyscheme.h
@@ -1,6 +1,12 @@
 #pragma once
 
-#define TOKEN_LEFT_PAREN "("
-#define TOKEN_RIGHT_PAREN ")"
+#define TOKEN_LEFT_PAREN '('
+#define TOKEN_RIGHT_PAREN ')'
 
+// space and tab
+// TODO: comment
 int skipBlank(std::string &input, int startIndex);
+// read a word as symbol
+int readSymbol(std::string &input, int startIndex);
+// a lisp form
+int readForm(std::string &input, int startIndex);
